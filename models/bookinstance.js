541651be0c9bf65f321870b5133151ba17208e35
YIFAN WU <wu0719900522@gmail.com>
2019-08-30 11:44:32 +1000

    Date formatting using moment

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -18,6 +19,12 @@ BookInstanceSchema
   .virtual('url')
   .get(function () {
     return '/catalog/bookinstance/' + this._id;
+  });
+  
+BookInstanceSchema
+  .virtual('due_back_formatted')
+  .get(function () {
+    return moment(this.due_back).format('MMMM Do, YYYY');
   });
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
